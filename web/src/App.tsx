4387e41dac6efe08acf03465cf9fcba6705d426f
daisy oliani <daisy.oliani@gmail.com>
2025-11-26 16:14:42 +0100

    chore(web): setup React + Vite + TS project and configure Tailwind v4

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 text-slate-50 p-8">
+      <h1 className="text-3xl font-bold">Brev.ly</h1>
+      <p className="mt-2 text-slate-300">
+        Tailwind v4 funcionando ✨
+      </p>
+    </div>
+  )
+}
+
+export default App
